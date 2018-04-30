10fa06de39c021f52802364c15412afd37fc5b81
catnipped <ossianboren@gmail.com>
2018-04-30 13:47:02 +0200

    dialog box

--- scripts.js
+++ scripts.js
@@ -1,10 +1,5 @@
 let carddata = new Object();
 
-$(document).ready(function() {
-	fetchCardData();
-	showAllCards();
-});
-
 function fetchCardData() {
 	$.getJSON('assets/DOM.json', function(data){
 		carddata = data.cards;
@@ -199,3 +194,38 @@ function toggleLowInk() {
         };
     })
 };
+
+function loadFile() {
+    var input, file, fr;
+
+    if (typeof window.FileReader !== 'function') {
+      alert("The file API isn't supported on this browser yet.");
+      return;
+    }
+
+    input = document.getElementById('fileinput');
+    if (!input) {
+      alert("Um, couldn't find the fileinput element.");
+    }
+    else if (!input.files) {
+      alert("This browser doesn't seem to support the `files` property of file inputs.");
+    }
+    else if (!input.files[0]) {
+      alert("Please select a file before clicking 'Load'");
+    }
+    else {
+      file = input.files[0];
+      fr = new FileReader();
+      fr.onload = receivedText;
+      fr.readAsText(file);
+    }
+
+    function receivedText(e) {
+      lines = e.target.result;
+      var newArr = JSON.parse(lines);
+			console.log(newArr)
+			carddata = newArr.cards;
+    }
+
+
+  }
